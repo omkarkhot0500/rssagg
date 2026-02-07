2e66cd0bc8ebeee775e96947d4c8f75b5872d334
omkarkhot0500 <omkarkhot0500@gmail.com>
2026-02-07 21:35:33 +0530

    add comments

--- handler_user.go
+++ handler_user.go
@@ -9,19 +9,31 @@ import (
 	"github.com/omkarkhot0500/rssagg/internal/database"
 )
 
+// handlerUsersCreate handles POST /users request
+// it reads user data from request body and saves it in database
 func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
+
+	// parameters struct defines expected JSON input format
+	// example input: { "name": "Omkar" }
 	type parameters struct {
 		Name string `json:"name"`
 	}
 
+	// Create JSON decoder to read request body
 	decoder := json.NewDecoder(r.Body)
+
+	// Empty struct to store decoded JSON
 	params := parameters{}
+
+	// Decode JSON request into params struct
 	err := decoder.Decode(&params)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
 		return
 	}
 
+	// Call database function to insert new user
+	// generate UUID and timestamps automatically
 	user, err := cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
@@ -33,5 +45,7 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, user)
+	// Convert DB user model into API response format
+	// then send JSON response back to client
+	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
 }
